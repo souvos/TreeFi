03fe941ba196684314476a20fe9a0a7a1703b21f
souvos <souvos0@gmail.com>
2022-02-28 02:36:55 +0600

    Added an error message

--- routes/authors.js
+++ routes/authors.js
@@ -16,7 +16,19 @@ router.get('/new', (req, res) => {
 // Create Author Route
 
 router.post('/', (req, res) => {
-    res.send(req.body.name)
+    const author = new Author({
+        name: req.body.name
+    })
+    author.save((err, newAuthor) => {
+        if (err) {
+            res.render('authors/new', {
+                author: author,
+                errorMessage: 'Erro creating Author'
+            })
+        } else {
+            res.redirect(`authors`)
+        }
+    })
 })
 
 module.exports = router
