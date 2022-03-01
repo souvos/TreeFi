13165e91d3dcfe664b37d24558910ff1edc2cbff
souvos <souvos0@gmail.com>
2022-03-01 18:54:52 +0600

    Fix book cover upload

--- routes/books.js
+++ routes/books.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const path = require('path')
+const fs = require('fs')
 const Book = require('../models/book')
 const Author = require('../models/author')
 const uploadPath = path.join('public', Book.coverImageBasePath)
 const multer = require('multer')
-const imageMimeTypes = ['images/jpeg', 'images/png', 'images/gif']
+const imageMimeTypes = ['image/jpeg', 'image/png', 'image/gif']
 const upload = multer({
     dest: uploadPath,
     fileFilter: (req, file, callback) => {
@@ -42,9 +43,18 @@ router.post('/', upload.single('cover'), async (req, res) => {
         //res.redirect(`books/${newBook.id}`)
         res.redirect(`books`)
     } catch {
+        if (book.coverImageName != null) {
+            removeBookCover(book.coverImageName)
+        }
         renderNewPage(res, book, true)
     }
 })
+
+function removeBookCover(fileName) {
+    fs.unlink(path.join(uploadPath, fileName), err => {
+        if(err) console.error(err)
+    })
+}
 
 async function renderNewPage(res, book, hasError = false) {
     try {
