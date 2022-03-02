12438997cbf61915632f1d55b8706238801d024b
souvos <souvos0@gmail.com>
2022-03-02 07:12:49 +0600

    Book Models and routes updated

--- routes/books.js
+++ routes/books.js
@@ -6,7 +6,7 @@ const Book = require('../models/book')
 const Author = require('../models/author')
 const uploadPath = path.join('public', Book.coverImageBasePath)
 const multer = require('multer')
-const imageMimeTypes = ['image/jpeg', 'image/png', 'image/gif']
+const imageMimeTypes = ['image/jpeg', 'image/jpg', 'image/png', 'image/gif']
 const upload = multer({
     dest: uploadPath,
     fileFilter: (req, file, callback) => {
@@ -16,7 +16,25 @@ const upload = multer({
 
 // All Books Route
 router.get('/', async (req, res) => {
-    res.send('All Books')
+    let query = Book.find()
+    if (req.query.title != null && req.query.title != '') {
+        query = query.regex('title', new RegExp(req.query.title, 'i'))
+    }
+    if (req.query.publishedBefore != null && req.query.publishedBefore != '') {
+        query = query.lte('publishDate', req.query.publishedBefore)
+    }
+    if (req.query.publishedAfter != null && req.query.publishedAfter != '') {
+        query = query.gte('publishDate', req.query.publishedAfter)
+    }
+    try {
+        const books = await query.exec()
+        res.render('books/index', {
+            books: books,
+            searchOptions: req.query
+        })
+    } catch {
+        res.redirect('/')
+    }
 })
 
 // New Book Route
